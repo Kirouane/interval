169c85432164bbda5d2aa2e7fedafef2fb416565
Nassim <cgmpnk@gmail.com>
2017-11-28 14:00:36 +0100

    parse  open/closed interval

--- tests/unit/Parser/IntervalParserTest.php
+++ tests/unit/Parser/IntervalParserTest.php
@@ -47,5 +47,83 @@ class IntervalParserTest extends \PHPUnit\Framework\TestCase
             $this->assertSame($start, $interval->getStart()->getValue());
             $this->assertSame($end, $interval->getEnd()->getValue());
         }
+
+        $this->assertSame(true, $interval->getStart()->isLeft());
+        $this->assertSame(true, $interval->getStart()->isClosed());
+
+        $this->assertSame(true, $interval->getEnd()->isRight());
+        $this->assertSame(true, $interval->getEnd()->isClosed());
+    }
+
+    public function parseExceptionProvider()
+    {
+        return [
+            [''],
+            ['invalid'],
+            ['[]'],
+            ['[,]'],
+            ['[,'],
+            [',]'],
+            ['[1]'],
+            ['[1 2]'],
+            ['[1 2'],
+            ['1 2]'],
+            ['1,2)'],
+            ['(1,2)'],
+            ['(1,2'],
+            ['[1,2'],
+            ['1,2]'],
+        ];
+    }
+
+    /**
+     * @test
+     * @expectedException \Exception
+     * @dataProvider parseExceptionProvider
+     * @param mixed $expression
+     */
+    public function parseException($expression)
+    {
+        $parser = new IntervalParser();
+        $parser->parse($expression);
+    }
+
+    public function parseOpenProvider()
+    {
+        return [
+            [']1,2[', 1, 2, true, true],
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider parseOpenProvider
+     * @param mixed $expression
+     * @param mixed $start
+     * @param mixed $end
+     * @param mixed $isOpenStart
+     * @param mixed $isOpenEnd
+     */
+    public function parseOpen($expression, $start, $end, $isOpenStart, $isOpenEnd)
+    {
+        $parser = new IntervalParser();
+
+        /** @var Interval $interval */
+        $interval = $parser->parse($expression);
+        $this->assertInstanceOf(Interval::class, $interval);
+        if ($start instanceof \DateTimeInterface) {
+            self::assertInstanceOf(\DateTimeInterface::class, $interval->getStart()->getValue());
+            $this->assertEquals($start, $interval->getStart()->getValue());
+            $this->assertEquals($end, $interval->getEnd()->getValue());
+        } else {
+            $this->assertSame($start, $interval->getStart()->getValue());
+            $this->assertSame($end, $interval->getEnd()->getValue());
+        }
+
+        $this->assertSame(true, $interval->getStart()->isLeft());
+        $this->assertSame($isOpenStart, $interval->getStart()->isOpen());
+
+        $this->assertSame(true, $interval->getEnd()->isRight());
+        $this->assertSame($isOpenEnd, $interval->getEnd()->isOpen());
     }
 }
